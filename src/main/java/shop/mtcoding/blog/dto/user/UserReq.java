3536f4737c088ff6054e1259091123228e80ec4b
Inhwa <2inhwa5@gmail.com>
2023-02-09 16:34:35 +0900

    모델링, 더미데이터 추가 및 회원가입 완료

--- src/main/java/shop/mtcoding/blog/dto/user/UserReq.java
+++ src/main/java/shop/mtcoding/blog/dto/user/UserReq.java
@@ -1,5 +1,17 @@
 package shop.mtcoding.blog.dto.user;
 
+import lombok.Getter;
+import lombok.Setter;
+
 public class UserReq {
 
+    @Getter
+    @Setter
+    public static class JoinReqDto {
+
+        private String username;
+        private String password;
+        private String email;
+
+    }
 }
