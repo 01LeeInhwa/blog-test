ae700a9a141d931db335a50b9a5022067622bf08
Inhwa <2inhwa5@gmail.com>
2023-02-09 16:59:03 +0900

    로그인 및 로그인 테스트 완료

--- src/main/java/shop/mtcoding/blog/dto/user/UserReq.java
+++ src/main/java/shop/mtcoding/blog/dto/user/UserReq.java
@@ -14,4 +14,12 @@ public class UserReq {
         private String email;
 
     }
+
+    @Getter
+    @Setter
+    public static class LoginReqDto {
+
+        private String username;
+        private String password;
+    }
 }
--- src/main/java/shop/mtcoding/blog/service/UserService.java
+++ src/main/java/shop/mtcoding/blog/service/UserService.java
@@ -2,8 +2,10 @@ package shop.mtcoding.blog.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import shop.mtcoding.blog.dto.user.UserReq.JoinReqDto;
+import shop.mtcoding.blog.dto.user.UserReq.LoginReqDto;
 import shop.mtcoding.blog.handler.ex.CustomException;
 import shop.mtcoding.blog.model.User;
 import shop.mtcoding.blog.model.UserRepository;
@@ -14,7 +16,8 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
-    public int 회원가입(JoinReqDto joinReqDto) {
+    @Transactional
+    public void 회원가입(JoinReqDto joinReqDto) {
 
         User sameUser = userRepository.findByUsername(joinReqDto.getUsername());
         if (sameUser != null) {
@@ -22,7 +25,19 @@ public class UserService {
         }
 
         int result = userRepository.insert(joinReqDto.getUsername(), joinReqDto.getPassword(), joinReqDto.getEmail());
-        return result;
+        if (result != 1) {
+            throw new CustomException("회원가입실패");
+        }
+    }
+
+    @Transactional(readOnly = true)
+    public User 로그인(LoginReqDto loginReqDto) {
+        User principal = userRepository.findByUsernameAndPassword(loginReqDto.getUsername(), loginReqDto.getPassword());
+        if (principal == null) {
+            throw new CustomException("username 혹은 password가 잘못되었습니다");
+        }
+
+        return principal;
     };
 
 }
