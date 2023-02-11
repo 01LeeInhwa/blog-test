22992dae6534e6c7b7f219c5281a9a71529dd14d
Inhwa <2inhwa5@gmail.com>
2023-02-11 21:08:10 +0900

    base64사진 여러장이 url 인코더 문제로 파싱 오류 발생해서 해결함

--- src/main/java/shop/mtcoding/blog/controller/BoardController.java
+++ src/main/java/shop/mtcoding/blog/controller/BoardController.java
@@ -68,22 +68,22 @@ public class BoardController {
     }
 
     @PostMapping("/board")
-    public String save(BoardSaveReqDto boardSaveReqDto) {
+    public @ResponseBody ResponseEntity<?> save(@RequestBody BoardSaveReqDto boardSaveReqDto) {
         User principal = (User) session.getAttribute("principal");
         if (principal == null) {
-            throw new CustomException("인증이 되지않았습니다.", HttpStatus.UNAUTHORIZED);
+            throw new CustomApiException("인증이 되지않았습니다.", HttpStatus.UNAUTHORIZED);
         }
         if (boardSaveReqDto.getTitle() == null || boardSaveReqDto.getTitle().isEmpty()) {
-            throw new CustomException("title을 작성해주세요");
+            throw new CustomApiException("title을 작성해주세요");
         }
         if (boardSaveReqDto.getContent() == null || boardSaveReqDto.getContent().isEmpty()) {
-            throw new CustomException("content을 작성해주세요");
+            throw new CustomApiException("content을 작성해주세요");
         }
         if (boardSaveReqDto.getTitle() == null || boardSaveReqDto.getTitle().isEmpty()) {
-            throw new CustomException("title의 길이가 100자 이하여야합니다");
+            throw new CustomApiException("title의 길이가 100자 이하여야합니다");
         }
         boardService.글쓰기(boardSaveReqDto, principal.getId());
-        return "redirect:/";
+        return new ResponseEntity<>(new ResponseDto<>(1, "긇쓰기 성공", null), HttpStatus.OK);
     }
 
     @GetMapping({ "/", "/board" })
--- src/main/java/shop/mtcoding/blog/service/BoardService.java
+++ src/main/java/shop/mtcoding/blog/service/BoardService.java
@@ -27,7 +27,7 @@ public class BoardService {
         int result = boardRepository.insert(boardSaveReqDto.getTitle(), boardSaveReqDto.getContent(),
                 thumbnail, userId);
         if (result != 1) {
-            throw new CustomException("글쓰기 실패", HttpStatus.INTERNAL_SERVER_ERROR);
+            throw new CustomApiException("글쓰기 실패", HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
